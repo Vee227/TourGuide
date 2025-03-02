514c34e0d27177c5311922fbe715239cba30f344
ViVictory11 <162926281+ViVictory11@users.noreply.github.com>
2025-03-02 19:39:45 +0100

    updates for structure

---
+++ TourGuide/TourGuide/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls.Primitives;
+using TourGuide.Comands;
+
+namespace TourGuide.ViewModels
+{
+    class MainWindowViewModel
+    {
+        private void DeleteItem()
+        {
+            if (SelectedItem != null)
+                Items.Remove(SelectedItem);
+        }
+
+        public RelayCommand 
+    }
+}
